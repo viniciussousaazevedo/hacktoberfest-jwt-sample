ff8cdd4ee18af61e432d7d36ed9a438ce2d8419c
viniciussousaazevedo <sousa0240@gmail.com>
2023-10-18 14:23:47 -0300

    initial commit

---
+++ src/main/java/com/jwt/sample/HacktoberfestJwtSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacktoberfestJwtSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HacktoberfestJwtSampleApplication.class, args);
+	}
+
+}
