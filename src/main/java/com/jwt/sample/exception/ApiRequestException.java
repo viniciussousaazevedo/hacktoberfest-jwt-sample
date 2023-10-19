49ae52eca55c544c197d78eaaf21b9f838aadcc4
viniciussousaazevedo <sousa0240@gmail.com>
2023-10-19 12:44:06 -0300

    add expeption package

---
+++ src/main/java/com/jwt/sample/exception/ApiRequestException.java
@@ -0,0 +1,12 @@
+package com.jwt.sample.exception;
+
+public class ApiRequestException extends RuntimeException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
