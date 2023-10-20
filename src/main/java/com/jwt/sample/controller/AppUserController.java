7c9124d8ad7e5a4cbfd0698d09bbfc8d995021a8
viniciussousaazevedo <sousa0240@gmail.com>
2023-10-20 13:08:27 -0300

    creates endpoint

--- src/main/java/com/jwt/sample/controller/AppUserController.java
+++ src/main/java/com/jwt/sample/controller/AppUserController.java
@@ -10,6 +10,9 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -35,7 +38,12 @@ public class AppUserController {
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
     }
 
-    // TODO: Who am I?
+    @GetMapping
+    public ResponseEntity<?> whoAmI() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        AppUser user = this.appUserService.getUser((String) authentication.getPrincipal());
+        return ResponseEntity.ok(user);
+    }
 
     // TODO: Forgot password
 
